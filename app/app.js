e547452312c3384f08a994e5fffe1b24a1d79ff8
Jesse Waddell <jesse@Jesses-MacBook-Pro.local>
2017-02-01 09:15:11 -0600

    added navbar and logout button/needs function

--- app/app.js
+++ app/app.js
@@ -1,3 +1,12 @@
 console.log('Ay sugga')
 
 const app = angular.module('re-diditApp', ['ngRoute', 'ngMaterial'])
+
+app.config(($routeProvider, $locationProvider) => {
+    $locationProvider.hashPrefix('')
+    $routeProvider
+        .when('/', {
+            controller: 'HomeCtrl',
+            templateUrl: 'partials/home.html',
+        })
+})
