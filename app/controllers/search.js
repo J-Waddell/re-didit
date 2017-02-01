1ce0b04b55b4b880733285741a7e7a82ab921f0b
Jesse Waddell <jesse@Jesses-MacBook-Pro.local>
2017-02-01 12:00:34 -0600

    added icon on login and fixed color scheme

--- app/controllers/search.js
+++ app/controllers/search.js
@@ -1,3 +1,13 @@
 app.controller('SearchCtrl', function($scope, $http, $location) {
     console.log('SearchCtrl')
 })
+
+// searchbar function
+app.config(['$mdThemingProvider', function($mdThemingProvider) {
+    $mdThemingProvider.theme('input')
+      .primaryPalette('amber')
+      .accentPalette('pink')
+      .warnPalette('red')
+      .backgroundPalette('grey');
+  }
+]);
