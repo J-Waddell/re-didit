5856c17b5ad0ec40cae926805b4d785b2dcce8e6
Jesse Waddell <jesse@Jesses-MacBook-Pro.local>
2017-02-01 11:25:13 -0600

    navbar functionality/logout reroutes to login/need to work on search

--- app/app.js
+++ app/app.js
@@ -2,11 +2,31 @@ console.log('Ay sugga')
 
 const app = angular.module('re-diditApp', ['ngRoute', 'ngMaterial'])
 
+    // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyD8cy9sR3jGmOFjiW54942podboOlj9Og0",
+    authDomain: "re-didit.firebaseapp.com",
+    databaseURL: "https://re-didit.firebaseio.com",
+    storageBucket: "re-didit.appspot.com",
+    messagingSenderId: "78197944126"
+  };
+  firebase.initializeApp(config);
+
 app.config(($routeProvider, $locationProvider) => {
     $locationProvider.hashPrefix('')
+    // $mdIconProvider.fontset('md', 'material-icons')
     $routeProvider
         .when('/', {
+            controller: 'LoginCtrl',
+            templateUrl: 'partials/login.html'
+        })
+        .when('/home', {
             controller: 'HomeCtrl',
             templateUrl: 'partials/home.html',
         })
+        .when('/search', {
+            controller: 'SearchCtrl',
+            templateUrl: 'partials/search.html'
+        })
+        .otherwise({redirectTo: '/'})
 })
---
+++ app/controllers/loginController.js
@@ -0,0 +1,31 @@
+app.controller('LoginCtrl', function ($scope, $location, $route, authFactory) {
+
+// sign in controller
+    $scope.login = () => {
+        console.log('working?')
+        authFactory.login($scope.email, $scope.password)
+      .then((response) => {
+        console.log(response)
+        $location.url('/home')
+      })
+    }
+  })
+
+
+// sign in factory
+.factory('authFactory', ($q) => {
+    return {
+      login (email, pass) {
+        return $q.resolve(firebase.auth().signInWithEmailAndPassword(email, pass))
+      },
+
+      getUserId () {
+        return firebase.auth().currentUser.uid
+      }
+    }
+  })
+// log out button clear data and re-route
+app.controller('LogoutController',function($location, $scope){
+    $window.localStorage.clear();
+    $location.path('/');
+});
